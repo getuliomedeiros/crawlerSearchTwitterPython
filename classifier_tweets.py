f3096af77fff8f4a1ab618b882bc473096d8df5c
Getúlio Medeiros <43413275+getuliomedeiros@users.noreply.github.com>
2019-10-31 15:47:18 -0300

    Update

--- classifier_tweets.py
+++ classifier_tweets.py
@@ -6,45 +6,42 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn import metrics
 from sklearn.model_selection import cross_val_predict
 
-fileWords = open("word_keys.txt", "r") 
-word_key = fileWords.readlines()
+#fileWords = open("word_keys.txt", "r") 
+#word_key = fileWords.readlines()
 
 from subprocess import check_output
 
 #for i in word_key:
 
-dataset = pd.read_csv(f'%23balburdia2.csv',encoding='utf-8')
+dataset = pd.read_csv('./tagsClassificadas.csv', encoding='utf-8')
 dataset.count()
 
-dataset[dataset.CLASSIFICACAO == '0'].count()
+dataset[dataset.Voto == 0].count()
 
-dataset[dataset.CLASSIFICACAO == '1'].count()
+dataset[dataset.Voto == 1].count()
 
-dataset[dataset.CLASSIFICACAO == '-1'].count()
+dataset[dataset.Voto == -1].count()
 
 dataset.head()
 
 tweets = dataset["text"].values.astype('U')
 tweets
 
-classes = dataset["CLASSIFICACAO"].values.astype('U')
+classes = dataset["Voto"].values.astype('U')
 classes
 
 vectorizer = CountVectorizer(analyzer = "word")
 freq_tweets = vectorizer.fit_transform(tweets)
 
-vectorizer = CountVectorizer(analyzer = "word")
-freq_tweets = vectorizer.fit_transform(tweets)
-
-modelo = MultinomialNB()
+modelo = MultinomialNB(alpha=1.0, class_prior=None, fit_prior=True)
 modelo.fit(freq_tweets, classes)
 
 testes = ["Esse governo está no início, vamos ver o que vai dar",
-        "Estou muito feliz com o governo de São Paulo esse ano",
-        "O estado de Minas Gerais decretou calamidade financeira!!!",
-        "A segurança desse país está deixando a desejar",
-        "O governador de Minas é do PT",
-        "O prefeito de São Paulo está fazendo um ótimo trabalho"]
+          "Polícia só não é bem-vinda onde acontecem crimes (estudantes reclamam por sofrer estupro em universidades);",
+          "O estado de Minas Gerais decretou calamidade financeira!!!",
+          "A segurança desse país está deixando a desejar",
+          "É um desgoverno literalmente contra a educação!! Vergonhoso!! ",
+          "Tbm com a presença do PT, PSOL, MST E SINDICATOS... FALTOU SÓ OS AMIGOS DAS FACÇÕES."]
 
 freq_testes = vectorizer.transform(testes)
 modelo.predict(freq_testes)
@@ -62,7 +59,7 @@ print(pd.crosstab(classes, resultados, rownames = ["Real"], colnames=["Predito"]
 vectorizer = CountVectorizer(ngram_range = (1, 2))
 freq_tweets = vectorizer.fit_transform(tweets)
 
-modelo = MultinomialNB()
+modelo = MultinomialNB(alpha=2.3, class_prior=None, fit_prior=True)
 modelo.fit(freq_tweets, classes)
 
 resultados = cross_val_predict(modelo, freq_tweets, classes, cv = 10)
